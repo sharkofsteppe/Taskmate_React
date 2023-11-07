d186df0a22175dfd4cfebb450c5d440ca61fc6b7
Bazr <79574109+sharkofsteppe@users.noreply.github.com>
2023-11-07 21:22:13 +0300

    second day progress

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import './App.css';
 import { Header } from './components/Header';
 import { ShowTask } from './components/ShowTask';
 import { AddTask } from './components/AddTask';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [tasklist, setTasklist] = useState([]);
+  const [task, setTask] = setState({});
+
   return (
     <div className="App">
       <Header />
-      <AddTask />
-      <ShowTask />
+      <AddTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+       />
+      <ShowTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+        />
     </div>
   );
 }
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,24 +1,24 @@
 
-export const ShowTask = () => {
-  const tasks = [{id: 10001, name: "TASK A", time: "2:09:01 AM 9/14/2030"},
-                  {id: 10001, name: "TASK A", time: "2:09:01 AM 9/14/2030"},
-                  {id: 10001, name: "TASK A", time: "2:09:01 AM 9/14/2030"}]
+export const ShowTask = ({tasklist, setTasklist, task, setTask},) => {
+
   return (
     <section className="showTask">
        <div className="head">
           <div>
             <span className="title">Todo</span>
-            <span className="count">0</span>
+            <span className="count">{tasklist.length}</span>
           </div>
-          <button className="clearAll">Clear All</button>
+          <button className="clearAll" onClick={ () => setTasklist([])}>Clear All</button>
        </div>
        <ul>
-          {tasks.map((task) => (
-            <li>
+          {tasklist.map((task) => (
+            <li key={task.id}>
               <p>
                   <span className="name">{task.name}</span>
                   <span className="time">{task.time}</span>
               </p>
+              <i className="bi bi-pencil-square"></i>
+              <i className="bi bi-trash"></i>
             </li>
           ))} 
        </ul>
