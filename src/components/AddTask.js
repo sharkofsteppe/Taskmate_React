fb43665b6225a608b5ca2aaaf87e8c2b87c5bec6
sharkofsteppe <bazr.ovalov@yandex.ru>
2023-11-09 14:40:16 +0000

    fix

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,20 +1,30 @@
 export const AddTask = ({tasklist, setTasklist, task, setTask}) => {
   const handleSubmit = (e) => {
     e.preventDefault();
-    const date = new Date();
-    const newTask = {
-      id: date.getTime(),
-      name: e.target.task.value,
-      time: `${date.toLocaleTimeString()} ${date.toLocaleDateString() }`
+    if (task.id) {
+      const date = new Date();
+      const updateTask = tasklist.map((todo) => (
+        todo.id === task.id ? {id: task.id, name: e.target.task.value, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo
+      ))
+      setTasklist(updateTask);
     }
-    setTasklist([...tasklist, newTask]);
-    e.target.task.value = "";
+    else {
+      const date = new Date();
+      const newTask = {
+        id: date.getTime(),
+        name: e.target.task.value,
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+      }
+      setTasklist([...tasklist, newTask]);
+      e.target.task.value = "";
+    }
+    
   }
 
   return (
     <section className="addTask">
       <form onSubmit={handleSubmit}>
-        <input type="text" name="task" value={task.name} autoComplete="off" placeholder="add task" maxLength="25"/>
+        <input type="text" name="task" value={task.name} autoComplete="off" placeholder="add task" maxLength="25" onChange={ e => setTask({...task, name: e.target.value})}/>
         <button type="submit">Add</button>
       </form>
     </section>
